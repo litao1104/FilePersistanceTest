421d6e05619d53fb104bd59ca7ef4de5e4582bc6
litao1104 <litao1104@gmail.com>
2020-11-01 00:11:06 +0800

    init code

---
+++ app/src/main/java/com/example/filepersistancetest/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.filepersistancetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edit = (EditText) findViewById(R.id.edit);
+
+        String inputText = load();
+        if (!TextUtils.isEmpty(inputText)) {
+            edit.setText(inputText);
+            edit.setSelection(inputText.length());
+            Toast.makeText(this, "Restoring succeeded", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("litao", "onDestroy1 ");
+
+        String inputText = edit.getText().toString();
+        Log.d("litao", "onDestroy2 " + inputText);
+        try {
+            Log.d("litao", "onDestroy3 " + inputText);
+            save(inputText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void save(String inputText) throws IOException {
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+
+        try {
+            out = openFileOutput("data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(inputText);
+            Log.d("litao", "save try " + inputText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
+
+    public String load() {
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+
+        try {
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return content.toString();
+    }
+}
